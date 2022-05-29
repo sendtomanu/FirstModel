688a8d0256639b202a01670f2c4892ad64171114
sendtomanu <89111267+sendtomanu@users.noreply.github.com>
2022-05-29 23:49:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, request, app, jsonify, url_for, render_template
 import numpy as np
 import pandas as pd
 
+
 app = Flask(__name__)
 model = pickle.load(open('/Users/manbirsingh/Downloads/pythonProject/model.pkl', 'rb'))
 
